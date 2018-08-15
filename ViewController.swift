6208a8b17492e90f1dd440cff7eaf84ab4314838
Victor Jimenez <vjuliojmnz@gmail.com>
2018-08-14 23:15:51 -0400

    Continued building model, touched up view controller

--- ViewController.swift
+++ ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
 
     }
     
-    var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1)/2)
+    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1)/2)
     
     var emojiChoices = ["👻", "🎃", "👻", "🎃"]
     
