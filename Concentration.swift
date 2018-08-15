64812e4eb20d151a04566ef10526d135e73026be
Victor Jimenez <vjuliojmnz@gmail.com>
2018-08-14 22:38:20 -0400

    Added class Concentration, it's the model for the app

---
+++ Concentration.swift
@@ -0,0 +1,17 @@
+//
+//  Concentration.swift
+//  ConcentrationGame
+//
+//  Created by Tracy on 8/14/18.
+//  Copyright © 2018 VictorInc. All rights reserved.
+//
+
+import Foundation
+
+class Concenration {
+    var card = [Card]
+    
+    func chooseCard(at index: Int) {
+        
+    }
+}
